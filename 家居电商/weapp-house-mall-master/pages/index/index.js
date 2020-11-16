44135fc5141a7119b4b4d216330d0ca01dfcf6bf
your name <your eamil>
2020-11-16 18:05:45 +0800

    1

--- 家居电商/weapp-house-mall-master/pages/index/index.js
+++ 家居电商/weapp-house-mall-master/pages/index/index.js
@@ -86,7 +86,28 @@ Page({
     ]
   },
   onLoad:function(){
-    
+
+   
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success (res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        wx.openLocation({
+          latitude,
+          longitude,
+          scale: 18,
+          name:"重庆市",
+          address:"要把钱",
+          success(res){
+            console.log(res)
+          }
+        })
+      } 
+     })
+
+
+
       var that = this;
       app.getUserInfo(function(userInfo){
           that.setData({userInfo:userInfo});
